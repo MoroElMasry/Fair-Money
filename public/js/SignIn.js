755c142c4e4624eeca4ecb806c011e1321b31eea
MoroElMasry <moroelmasry507@gmail.com>
2022-12-17 23:59:38 +0200

    finalCommit

--- public/js/SignIn.js
+++ public/js/SignIn.js
@@ -1,17 +1,25 @@
 function validate()
 {
     var m = document.getElementById("emailAddress").value;
-    var mREGEX = /^[a-zA-Z0-9_\.]{2,}@[a-zA-Z]+.[a-zA-Z]+$/;
-    if (mREGEX.test(m) == false) {
-      alert("Please enter a valid e-mail!");
-      // swal("Oops! Wrong Format!", "Please enter a valid e-mail!", "error");
-      return false;
-    }
     var p = document.getElementById("password").value;
-    var pRegex = /^([a-zA-Z$_0-9]){8,32}$/;
-    if (pRegex.test(p) == false) {
-      alert("please enter a valid password");
-      // swal("Oops! Wrong Format!", "please enter a valid password!", "error");
-      return false;
+    if(m =='' || p =='')
+    {
+      alert("Please Fill The Missing Fields!")
     }
+    else
+    {
+      var mREGEX = /^[a-zA-Z0-9_\.]{2,}@[a-zA-Z]+.[a-zA-Z]+$/;
+      if (mREGEX.test(m) == false) {
+        alert("Please enter a valid e-mail!");
+        // swal("Oops! Wrong Format!", "Please enter a valid e-mail!", "error");
+        return false;
+      }
+      var pRegex = /^([a-zA-Z$_0-9]){8,32}$/;
+      if (pRegex.test(p) == false) {
+        alert("please enter a valid password");
+        // swal("Oops! Wrong Format!", "please enter a valid password!", "error");
+        return false;
+      }
+    }
+    
   }
