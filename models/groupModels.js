dd017eae0e41494b24e8615bc22885f2c5e77fcd
peter nady <nady.peter347@gmail.com>
2022-12-24 03:24:57 +0200

    autocomplete feature code

---
+++ models/groupModels.js
@@ -0,0 +1,3 @@
+// tourSchema.virtual('durationWeeks').get(function() {
+//   return this.duration / 7;
+// });
--- public/js/CreateNewGroup.js
+++ public/js/CreateNewGroup.js
@@ -1,35 +1,36 @@
 "use strict";
 
 const forms = document.querySelector(".needs-validation");
+const membersInput = document.querySelector("#grpMembers");
+membersInput.addEventListener("input", () => {
+  console.log("object");
+});
 
-function validate()
-{
+function validate() {
   var grpN = document.getElementById("grpName").value;
   var GrpDesc = document.getElementById("grpDescription").value;
   var nREGEX = /^[a-zA-Z\s]{3,}$/; // at least lower 3 upper space
 
-  if(grpN =='' || GrpDesc =='')
-  {
+  if (grpN == "" || GrpDesc == "") {
     // alert("Please Fill The Missing Fields!");
     swal("Some Field is Empty!", "Please fill the next fields!", "warning");
     return false;
-  }
-  else
-  {
-    if (nREGEX.test(grpN) == false) 
-    {
+  } else {
+    if (nREGEX.test(grpN) == false) {
       // alert("Please enter a valid Group Name");
       swal("Oops! Wrong Format!", "Please enter a valid Group name!", "error");
       return false;
     }
-    
+
     var mREGEX = /^[a-zA-Z\s]{3,}$/; // at least 3 lower upper space
-    if (mREGEX.test(GrpDesc) == false) 
-    {
+    if (mREGEX.test(GrpDesc) == false) {
       // alert("Please enter a valid Group Description!");
-      swal("Oops! Wrong Format!", "Please enter a valid Group Description!", "error");
+      swal(
+        "Oops! Wrong Format!",
+        "Please enter a valid Group Description!",
+        "error"
+      );
       return false;
     }
   }
-  
 }
